59ff2074977e07f5d234ac7cead6de615310f917
tynr426 <tynr426@126.com>
2020-08-27 15:27:40 +0800

    redis hash

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
 use redis::{Commands, ToRedisArgs};
-use std::collections::{BTreeMap, BTreeSet, HashMap};
+use std::collections::{BTreeMap, BTreeSet};
 
 fn main() {
     println!("Hello, world!");
 
     println!("{:?}", do_something());
 }
-const REDIS_URL: &'static str = "redis://127.0.0.1:6379/4";
-fn set_dic<T: ToRedisArgs + Clone>(map: BTreeMap<String, T>) {
-    let client = redis::Client::open(REDIS_URL).unwrap();
-
+fn set_hashs<K: AsRef<str>, T: ToRedisArgs + Copy>(map: BTreeMap<K, BTreeMap<T, T>>) {
+    let client = redis::Client::open("redis://127.0.0.1:6379/0").unwrap();
     let conn = client.get_connection();
     if conn.is_err() {
         return;
     }
     let mut conn = conn.unwrap();
     // redis::pipe().set_multiple(map);
-    for (key, val) in map.iter() {
-        let v: () = redis::pipe()
-            .cmd("SADD")
-            .set(key, val.clone())
-            .query(&mut conn)
-            .unwrap_or(());
+    println!("{}", map.len());
+    for (key, h) in map.iter() {
+        for (f, val) in h {
+            match redis::pipe()
+                .cmd("HSET")
+                .arg(key.as_ref())
+                .arg(f.clone())
+                .arg(val.clone())
+                .query(&mut conn)
+            {
+                Ok(v) => v,
+                Err(_) => {}
+            }
+        }
     }
 }
 fn do_something() -> redis::RedisResult<()> {
-    let client = redis::Client::open(REDIS_URL).unwrap();
+    let client = redis::Client::open("redis://127.0.0.1:6379/0").unwrap();
     let mut con = client.get_connection().unwrap();
     let mut v: BTreeMap<String, BTreeMap<u32, u32>> = BTreeMap::new();
     let mut dic: BTreeMap<u32, u32> = BTreeMap::new();
@@ -33,23 +39,30 @@ fn do_something() -> redis::RedisResult<()> {
     dic.insert(9, 1);
     dic.insert(10, 1);
     v.insert("18981772611".to_owned(), dic);
-    set_dic(v);
+    set_hashs(v);
     // con.set("key1", b"foo");
     // redis::cmd("SET").arg("my_key2").arg(b"testatatat").execute(&mut con);
-    // for (key, val) in v.iter() {
+    // for (key, h) in v.iter() {
     //     println!("redis cache={}", key);
-    //     let v: () = redis::pipe()
-    //         .cmd("SADD")
-    //         .set(key, val.clone())
-    //         .query(&mut con)
-    //         .unwrap_or(());
+    //     for (f, val) in h {
+    //         let v: () = redis::pipe()
+    //             .cmd("HSET")
+    //             .arg(key)
+    //             .arg(*f)
+    //             .arg(*val)
+    //             // .hset("store", key, val.clone())
+    //             .query(&mut con)
+    //             .unwrap();
+    //     }
     // }
     // 采用命令的方式也可以写入,测试成功
 
     // 采用con.sadd的方式也可以写入成功
     // let is_ok = con.sadd("ky5", v)?;
+    println!("{:?}", 2);
+
     // 获取写入的set值
-    let x: BTreeMap<u32, u32> = con.smembers("18981772611")?;
+    let x: BTreeMap<u32, u32> = con.hgetall("18981772611")?;
     println!("{:?}", x);
 
     // println!("{:?}", con);
