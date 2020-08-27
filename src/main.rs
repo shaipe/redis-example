4952f634ba0b336427eed65c66f7e17cc265987f
tynr426 <tynr426@126.com>
2020-08-27 08:56:01 +0800

    范型

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,54 @@
-use redis::Commands;
-use std::collections::{HashMap, HashSet, BTreeMap};
+use redis::{Commands, ToRedisArgs};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 
 fn main() {
     println!("Hello, world!");
 
-   println!("{:?}", do_something());
+    println!("{:?}", do_something());
 }
+fn set_dic<T: ToRedisArgs + Clone>(map: BTreeMap<String, T>) {
+    let client = redis::Client::open("redis://127.0.0.1:6379/4").unwrap();
 
+    let conn = client.get_connection();
+    if conn.is_err() {
+        return;
+    }
+    let mut conn = conn.unwrap();
+    // redis::pipe().set_multiple(map);
+    for (key, val) in map.iter() {
+        let v: () = redis::pipe()
+            .cmd("SADD")
+            .set(key, val.clone())
+            .query(&mut conn)
+            .unwrap_or(());
+    }
+}
 fn do_something() -> redis::RedisResult<()> {
-    let client = redis::Client::open("redis://:redis.root@127.0.0.1:6379/0").unwrap();
-    
+    let client = redis::Client::open("redis://127.0.0.1:6379/4").unwrap();
     let mut con = client.get_connection().unwrap();
-    let mut v: BTreeMap<u32, u32> = BTreeMap::new();
-    v.insert(2, 5);
-    v.insert(4, 6);
+    let mut v: BTreeMap<String, BTreeMap<u32, u32>> = BTreeMap::new();
+    let mut dic: BTreeMap<u32, u32> = BTreeMap::new();
+    dic.insert(8, 1);
+    dic.insert(9, 1);
+    dic.insert(10, 1);
+    v.insert("18981772611".to_owned(), dic);
+    set_dic(v);
     // con.set("key1", b"foo");
     // redis::cmd("SET").arg("my_key2").arg(b"testatatat").execute(&mut con);
-
+    // for (key, val) in v.iter() {
+    //     println!("redis cache={}", key);
+    //     let v: () = redis::pipe()
+    //         .cmd("SADD")
+    //         .set(key, val.clone())
+    //         .query(&mut con)
+    //         .unwrap_or(());
+    // }
     // 采用命令的方式也可以写入,测试成功
-    redis::pipe().cmd("SADD").arg("key6").arg(v).execute(&mut con);
 
     // 采用con.sadd的方式也可以写入成功
     // let is_ok = con.sadd("ky5", v)?;
-    // println!("{:?}", is_ok);
-
     // 获取写入的set值
-    let x: BTreeMap<u32, u32> = con.smembers("key6")?;
+    let x: BTreeMap<u32, u32> = con.smembers("18981772611")?;
     println!("{:?}", x);
 
     // println!("{:?}", con);
